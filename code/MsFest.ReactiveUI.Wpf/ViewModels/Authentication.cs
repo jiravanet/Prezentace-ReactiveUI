13e08298c6476fde2ef5b2e4d3ccca6962381074
Jarda Jirava <jarda@jirava.net>
2013-11-02 16:04:17 +0100

    code with ReactiveObject, ReactiveCommand, code behind binding

---
+++ code/MsFest.ReactiveUI.Wpf/ViewModels/Authentication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MsFest.ReactiveUI.Wpf.ViewModels
+{
+    public interface IAuthentication
+    {
+        AuthenticationResult Authenticate(string username, string password);
+        Task<AuthenticationResult> AuthenticateAsync(string username, string password);
+    }
+
+    public class Authentication : IAuthentication
+    {
+        public AuthenticationResult Authenticate(string username, string password)
+        {
+            return username == "jarda" && password == "heslo"
+                       ? AuthenticationResult.Authenticated
+                       : AuthenticationResult.Nonauthenticated;
+        }
+
+        public Task<AuthenticationResult> AuthenticateAsync(string username, string password)
+        {
+            var tcs = new TaskCompletionSource<AuthenticationResult>();
+            new TaskFactory().StartNew(() =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(2)).
+                     ContinueWith(t => tcs.SetResult(Authenticate(username,
+                         password)));
+            });
+            return tcs.Task;
+        }
+    }
+
+    public enum AuthenticationResult
+    {
+        Authenticated,
+        Nonauthenticated
+    }
+}
---
+++ code/MsFest.ReactiveUI.Wpf/ViewModels/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+
+namespace MsFest.ReactiveUI.Wpf.ViewModels
+{
+    public class LoginViewModel : ReactiveObject
+    {
+        public LoginViewModel()
+        {
+            var authentication = new Authentication();
+            var canLogin = this.WhenAny(x => x.LoginName,
+                x => x.Password,
+                (l, p) => !String.IsNullOrWhiteSpace(l.Value) && !String.IsNullOrWhiteSpace(p.Value));
+            LoginCommand = new ReactiveCommand(canLogin);
+            var loggedIn = LoginCommand.RegisterAsyncTask(_ => authentication.AuthenticateAsync(LoginName,
+                Password).
+                                                                              ContinueWith(t => t.Result == AuthenticationResult.Authenticated
+                                                                                                    ? "Přihlášen"
+                                                                                                    : "Nepřihlášen"));
+            message = new ObservableAsPropertyHelper<string>(loggedIn,
+                s => raisePropertyChanged("Message"));
+        }
+
+        string loginName;
+
+        public string LoginName
+        {
+            get { return loginName; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref loginName,
+                    value);
+            }
+        }
+
+        string password;
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref password,
+                    value);
+            }
+        }
+
+        ObservableAsPropertyHelper<string> message;
+
+        public string Message
+        {
+            get { return message.Value; }
+        }
+
+        public ReactiveCommand LoginCommand { get; protected set; }
+    }
+}
